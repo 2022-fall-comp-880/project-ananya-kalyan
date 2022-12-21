911254982340b84beac26d0180982fc2f375d767
ab1791 <ab1791@unh.edu>
2022-12-21 18:48:13 -0500

    fix docstring errors

--- Apps/company_founded_year_count.py
+++ Apps/company_founded_year_count.py
@@ -1,5 +1,6 @@
 """
-company_founded_year_count.py
+Represents a data_set of US_startup Companies over time information.
+
 Authors:
   - Ananya Bojja
   - Kalyan Kumar Gade
@@ -16,6 +17,7 @@ class CompanyFoundedYear:
     Attributes:
         filename: string
     """
+
     def __init__(self, filename):
         """
         Instance variable initialization.
@@ -26,10 +28,14 @@ class CompanyFoundedYear:
 
     def read_data_file_to_d(self, value: int) -> list:
         """
+        Read data from a text file into a dict.
+
         Reads from text file named `self.filename` using `csv.reader()` method
         and creates a dictionary with the data in column key_file as keys and
         data in column value_file as values.
+
         :param value: non-negative integer.
+
         :return: list
             value: list of integers, list of founded year of the companies.
         """
@@ -42,32 +48,39 @@ class CompanyFoundedYear:
                     year_list.append(key)
         return year_list
 
-
-    def company_social_accounts(self):
+    def company_founded_year(self):
         """
+        Create a dictionary of company founded year with count.
+
         Creates and returns a dictionary whose keys are founded year of
-        and values are no of companies founded in the year corresponding to that year.
+        and values are no of companies founded in the year corresponding to
+        that year.
+
         :return: dictionary
             key: integer, represents the year founded
-            value: integer, represents the no of companies founded in that year.
+            value: integer, represents the no of companies founded in
+            that year.
         """
-        no_company_socials = 4
-        comp_social_accounts_dict = self.read_data_file_to_d(no_company_socials)
-        return comp_social_accounts_dict
-
+        founded_year = 4
+        comp_founded_year_dict = self.read_data_file_to_d(founded_year)
+        return comp_founded_year_dict
 
     def no_of_companies_founded(self):
         """
-        Gets dictionary of year founded and voting average by calling
-            self.company_social_accounts().
+        Create a dictionary of year founded and no of companies founded.
+
+        Gets dictionary of year founded and no of companies founded
+         by calling self.company_social_accounts().
+
         Creates and returns a new dictionary whose keys are the year founded
-            and value is total no of companies founded in specific year
+            and value is total no of companies founded in specific year.
+
         :returns: dictionary
            key: int, founded_year
            value: int, no of companies founded in specific year
         """
         companies_founded_year_d = {}
-        year = self.company_social_accounts()
+        year = self.company_founded_year()
         for no_companies in year:
             if no_companies == "-1":
                 if "unknown" in companies_founded_year_d:
@@ -80,7 +93,3 @@ class CompanyFoundedYear:
                 else:
                     companies_founded_year_d[no_companies] = 1
         return companies_founded_year_d
-
-
-
-
