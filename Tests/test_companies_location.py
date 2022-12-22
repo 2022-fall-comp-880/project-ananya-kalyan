b13ffb65e4ec8829bd13f5ef3eedae7e50e1b818
ab1791 <ab1791@unh.edu>
2022-12-21 20:23:06 -0500

    fix pylint errors

--- Tests/test_companies_location.py
+++ Tests/test_companies_location.py
@@ -13,16 +13,18 @@ class TestCompaniesLocation(unittest.TestCase):
         """Create objects for the three testing cases."""
         data_dir = os.path.dirname(__file__) + "/../data"
         self.startups_stats = CompanyLocation(f'{data_dir}/Startup_data.csv')
-        self.startups_stats_empty = CompanyLocation(f'{data_dir}/Startup_data_empty.csv')
-        self.startups_stats_First5 = CompanyLocation(f'{data_dir}/Startup_data_First5.csv')
-        self.startups_stats_Last10 = CompanyLocation(f'{data_dir}/Startup_data_Last10.csv')
+        self.startups_stats_empty = CompanyLocation(
+            f'{data_dir}/Startup_data_empty.csv')
+        self.startups_stats_First5 = CompanyLocation(
+            f'{data_dir}/Startup_data_First5.csv')
+        self.startups_stats_Last10 = CompanyLocation(
+            f'{data_dir}/Startup_data_Last10.csv')
 
     def test_multiple_entries(self):
         """Test case 1 using Startup_data.csv."""
         actual_res = self.startups_stats.company_loc_by_team_size()
         print(self.startups_stats.location_by_company_team_size())
         print(actual_res)
-
 
     def test_empty(self):
         """Test case 2 using Startup_data_empty.csv."""
@@ -31,7 +33,6 @@ class TestCompaniesLocation(unittest.TestCase):
         print(actual_res1)
         expected_res1 = {}
         self.assertNotEqual(actual_res1, expected_res1)
-
 
     def test_first_five_entries(self):
         """Test case 3 using Startup_data_First5.csv."""
@@ -43,10 +44,8 @@ class TestCompaniesLocation(unittest.TestCase):
                               'Dropbox']}
         self.assertNotEqual(actual_res2, expected_res2)
 
-
     def test_last_ten_entries(self):
         """Test case 4 using Startup_data_Last10."""
         actual_res3 = self.startups_stats_Last10.company_loc_by_team_size()
         print(self.startups_stats_Last10.location_by_company_team_size())
         print(actual_res3)
-
